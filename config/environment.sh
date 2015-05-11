1a6eb79bc003cf84bf5074bbda51c545371b9a51
zesbr <jesse_ruuth@hotmail.com>
2015-05-11 15:34:10 +0300

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="jesruuth"
 # korvaa tämä haluamallasi kansion nimellä
 PROJECT_FOLDER="tsoha"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
